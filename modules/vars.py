c613c442c1e0701cb0c05af93e7e8766fadbd643
buzihelp-source <buzihelp@gmail.com>
2025-12-22 09:12:04 +0530

    Update default PORT value and formatting in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 31138834))
-API_HASH = os.environ.get("API_HASH", "a6e6098b319618f10693005ee7a77d92")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8551803950:AAEVdjf6EqoAlg21FEaxjQBJx0DrZsiyBak")
+API_ID    = os.environ.get("API_ID", "31138834")
+API_HASH  = os.environ.get("API_HASH", "a6e6098b319618f10693005ee7a77d92")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8551803950:AAEVdjf6EqoAlg21FEaxjQBJx0DrZsiyBak") 
 
 WEBHOOK = True  # Don't change this
-PORT = int(os.environ.get("PORT", 8080)) # Default to 8080 if not set
+PORT = int(os.environ.get("PORT", 8870))  # Default to 8000 if not set
 
