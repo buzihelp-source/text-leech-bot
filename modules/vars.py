6dfe7110487362c9fee5a852c0a11c2154de8646
buzihelp-source <buzihelp@gmail.com>
2025-12-24 08:16:15 +0530

    Change default PORT from 8870 to 8080

--- modules/vars.py
+++ modules/vars.py
@@ -5,5 +5,5 @@ API_HASH  = os.environ.get("API_HASH", "a6e6098b319618f10693005ee7a77d92")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8551803950:AAEVdjf6EqoAlg21FEaxjQBJx0DrZsiyBak") 
 
 WEBHOOK = True  # Don't change this
-PORT = int(os.environ.get("PORT", 8870))  # Default to 8000 if not set
+PORT = int(os.environ.get("PORT", 8080))
 
